e871d57c638fbe72da63f360d9278ddb6520997a
Celine Chan <cctinlone@gmail.com>
2023-08-21 13:51:18 +0200

    not able to loop through

---
+++ src/components/Poke_List.vue
@@ -0,0 +1,35 @@
+<template>
+  <h1>Begining Pokemon List!</h1>
+  <div class="poke-list">
+    <div v-if="!pokemonData">Loading data...</div>
+    <pre v-else><Poke_Card />
+    {{ pokemonData.results}} 
+  </pre>
+  </div>
+</template>
+
+<script>
+import Poke_Card from './Poke_Card.vue'
+
+export default {
+  components: {
+    Poke_Card
+  },
+  data() {
+    return {
+      pokemonData: []
+    }
+  },
+  methods: {
+    async fetchData() {
+      const response = await fetch('https://pokeapi.co/api/v2/pokemon')
+      this.pokemonData = await response.json()
+      console.log(this.pokemonData.results)
+      console.log(this.pokemonData.results[0].name)
+    }
+  },
+  mounted() {
+    this.fetchData()
+  }
+}
+</script>
